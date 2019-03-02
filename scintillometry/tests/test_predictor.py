9afbab1cb299bdab0d43db754b9b6d12adf62fc0
Jing Luo <luojing1211@gmail.com>
2019-03-01 21:16:42 -0500

    update test predictor

--- scintillometry/tests/test_predictor.py
+++ scintillometry/tests/test_predictor.py
@@ -39,11 +39,15 @@ class TestPredictor:
         f1 = self.polyco(time, deriv=2, time_unit=u.min)
         assert len(f1) == len(time)
         assert f1.unit == u.cycle/(u.min**2)
-        pr = self.polyco(time, rphase='fraction')
+        pr0 = self.polyco(self.polyco[0]['mjd_mid'], rphase=0)
+        assert pr0 == 0.0 * u.cycle, "`reference phase` is not setup right."
+        pf = self.polyco(self.polyco['mjd_mid'], rphase='fraction')
+        assert np.all(pf.value - self.polyco['rphase'] % 1.0  < 2e-5), \
+            "`reference phase` is not setup right."
 
         # Test mjd input
         time2 = (self.start_time.mjd +
-                (np.linspace(0, 100) * 10.0 * u.min).to(u.day).value)
+                 (np.linspace(0, 100) * 10.0 * u.min).to(u.day).value)
         p = self.polyco(time2[0])
         assert p.shape == ()
         # Test array input
@@ -53,4 +57,4 @@ class TestPredictor:
     def test_over_range(self):
         time = self.start_time + 10 * u.day
         with pytest.raises(ValueError):
-            p = self.polyco(time)
+            self.polyco(time)
