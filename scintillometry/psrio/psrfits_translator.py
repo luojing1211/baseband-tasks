b43b845777932dce9e843f9ffa2e51ec9b7135cb
Jing Luo <luojing1211@gmail.com>
2019-03-23 20:15:09 -0400

    Make translator class a astropy fits style

--- scintillometry/psrio/psrfits_translator.py
+++ scintillometry/psrio/psrfits_translator.py
@@ -5,12 +5,15 @@ object and psrfits file hdus.
 from .translator import TranslatorBase
 from collections import namedtuple
 import astropy.units as u
+from astropy.time import Time
+from astropy.io import fits
+import numpy as np
 
 
-__all__ = [HeaderHDU, SubintHDU]
+__all__ = ["Psrfits", "SubintHDU"]
 
 
-class HeaderHDU(TranslatorBase):
+class PsrfitsHearderHDU(fits.PrimaryHDU):
     """HeaderHDU class provides the translator function between baseband-style
     file object and the PSRFITS main header HDU.
 
@@ -18,18 +21,18 @@ class HeaderHDU(TranslatorBase):
     ---------
     header_hdu : `Pdat.header_hdu`
     """
-    _properties = ('start_time', 'observatory', 'filename')
+    _properties = ('start_time', 'observatory', 'frequency')
 
-    _defaults = [('HDRVER', '6.1 ' 'Header version')
-                 ('FITSTYPE', 'PSRFITS ', 'FITS definition for pulsar data'
+    _defaults = [('HDRVER', '6.1', 'Header version'),
+                 ('FITSTYPE', 'PSRFITS', 'FITS definition for pulsar data'
                   'files'),
                  ('DATE', ' ', 'File creation UTC date (YYYY-MM-DDThh:mm:ss)'),
                  ('OBSERVER', ' ', 'Observer name(s)'),
                  ('PROJID', ' ', 'Project name'),
                  ('TELESCOP', ' ', 'Telescope name'),
-                 ('ANT_X', 0  '[m] Antenna ITRF X-coordinate (D)'),
-                 ('ANT_Y', 0  '[m] Antenna ITRF Y-coordinate (D)'),
-                 ('ANT_Z', 0  '[m] Antenna ITRF Z-coordinate (D)'),
+                 ('ANT_X', 0,  '[m] Antenna ITRF X-coordinate (D)'),
+                 ('ANT_Y', 0,  '[m] Antenna ITRF Y-coordinate (D)'),
+                 ('ANT_Z', 0,  '[m] Antenna ITRF Z-coordinate (D)'),
                  ('FRONTEND', ' ', 'Receiver ID'),
                  ('IBEAM', ' ', 'Beam ID for multibeam systems'),
                  ('NRCVR', 0, 'Number of receiver polarisation channels'),
@@ -73,24 +76,33 @@ class HeaderHDU(TranslatorBase):
                  ('CAL_NPHS', 0, 'Number of states in cal pulse (I)'),
                  ('STT_IMJD', 0, 'Start MJD (UTC days) (J - long integer)'),
                  ('STT_SMJD', 0, '[s] Start time (sec past UTC 00h) (J)'),
-                 ('STT_OFFS', 0, '[s] Start time offset (D)'),
+                 ('STT_OFFS', 0.0, '[s] Start time offset (D)'),
                  ('STT_LST', 0, '[s] Start LST (D)')]
 
-    def __init__(self, header_hdu, mode):
-        self.header_hdu = header_hdu
-        self.verify(self.header_hdu)
-        super(HeaderHDU, self).__init__('psrfits', 'baseband',
-                                        'psrfits_header', mode)
+    def __init__(self, header_hdu=None):
+        if header_hdu is None:
+            super(PsrfitsHearderHDU, self).__init__()
+            self._init_empty()
+        else:
+            super(PsrfitsHearderHDU, self).__init__(header=header_hdu.header,
+                                                    data=header_hdu.data)
+            self.verify()
 
-    def verify(self, input_hdu):
-        try:
-            self.header =  input_hdu.read_header()
-            assert self.header['SIMPLE']
-        except:
-            raise ValueError("Input HDU is not a main header HDU.")
+    def verify(self):
+        if self.header['FITSTYPE'] != "PSRFITS":
+            raise ValueError("The input fits header is not a PSRFITS type.")
+
+    def _init_empty(self):
+        for k in self._defaults:
+            self.header.set(k[0], k[1], k[2])
+        self.header.add_comment('FITS (Flexible Image Transport System) format'
+                                'is defined in Astronomy and Astrophysics, '
+                                'volume 376, page 359; bibcode:'
+                                ' 2001A&A...376..359H', after='EXTEND')
 
     @property
     def start_time(self):
+        # TODO add location
         MJD_d_int = self.header['STT_IMJD'] * u.day
         MJD_s_int = self.header['STT_SMJD'] * u.s
         MJD_s_frac = self.header['STT_OFFS'] * u.s
@@ -100,10 +112,11 @@ class HeaderHDU(TranslatorBase):
 
     @start_time.setter
     def start_time(self, time):
+        # TODO add location
         if not isinstance(time, Time):
             raise ValueError("Input time should be an `astropy.time.Time`"
                              " object.")
-        if time.isisscalar:
+        if not time.isscalar:
             raise ValueError("Input time should be a scalar")
         else:
             self.header['STT_IMJD'] = int(divmod(time.mjd, 1)[0])
@@ -113,21 +126,47 @@ class HeaderHDU(TranslatorBase):
             int_sec, frac_sec = divmod(dt.value, 1)
             self.header['STT_SMJD'] = int(int_sec)
             self.header['STT_OFFS'] = frac_sec
+            self.header['DATE-OBS'] = time.isot.split('.')[0]
+
+    @property
+    def observatory(self):
+        return self.header['TELESCOP']
+
+    @observatory.setter
+    def observatory(self, val):
+        self.header['TELESCOP'] = val
+        #TODO ADD ITRF coordinate.
+
+    @property
+    def frequency(self):
+        n_chan = self.header['OBSNCHAN']
+        c_chan = self.header['OBSFREQ']
+        bw = self.header['OBSBW']
+        chan_bw = bw / n_chan
+        #NOTE the frequency marker is on the center of the channels
+        freq = np.arange(-n_chan / 2, n_chan / 2) * chan_bw + c_chan
+        return u.Quantity(freq, u.MHz, copy=False)
+
+    @frequency.setter
+    def frequency(self, frequency):
+        #NOTE this assumes the frequency marker is on the center of the channels
+        if hasattr(frequency, 'unit'):
+            frequency = frequency.to(u.MHz)
+        else:
+            frequency = u.Quantity(frequency, u.MHz, copy=False)
+        n_chan = len(frequency)
+        self.header['OBSNCHAN'] = n_chan
+        self.header['OBSBW'] = (frequency[-1] - frequency[0]).value
+        self.header['OBSFREQ'] = frequency[int(n_chan / 2)].value
 
 
-    def get_filename(self):
-        return self.header_hdu.get_filename()
-
-
-class SubintHDU(TranslatorBase):
+class SubintHDU(fits.BinTableHDU):
     """SubintHDU class provides the translator functions between baseband-style
     file object and the PSRFITS SUBINT HDU.
 
     Parameter
     ---------
-    name : str
-        The name of the translator instance.
-    header_hdu : object
+    header_hdu : PsrfitsHearderHDU
         The psrfits main header object
     subint_hdu : HDU oject
         The psrfits data HDU.
@@ -139,103 +178,115 @@ class SubintHDU(TranslatorBase):
     """
 
     _properties = ('start_time', 'sample_rate', 'shape', 'pol', 'frequency')
+    _defaults = [('BITPIX', 8, 'N/A'),
+                 ('NAXIS', 2, '2-dimensional binary table'),
+                 ('NAXIS1', 0, 'width of table in bytes'),
+                 ('NAXIS2', 0, 'Number of rows in table (NSUBINT)'),
+                 ('PCOUNT', 0, 'size of special data area'),
+                 ('GCOUNT', 1, 'one data group (required keyword)'),
+                 ('TFIELDS', 18, 'Number of fields per row')]
 
 
-    _buffer = [('OFFS_SUB', 0),]
-
-    def __init__(self, header_hdu, subint_hdu, mode):
-        self.header_hdu = HeaderTranslator("file_header", header_hdu)
-        self.data_hdu = data_hdu
-        self.data_header = self.data_hdu.read_header()
-        super(SubintHDU, self).__init__('psrfits','baseband', 'SUBINT', mode)
-        self.verify(self.data_hdu)
-
-    def verify(self, input_hdu):
-        if input_hdu.get_filename() != self.header_hdu.get_filename():
-            raise ValueError("Main header HDU and input HDU are not from the "
-                             "same file.")
+    def __init__(self, header_hdu, subint_hdu=None):
+        self.header_hdu = header_hdu
+        if subint_hdu is None:
+            super(SubintHDU, self).__init__()
+            self._init_empty()
         else:
-            if input_hdu.get_extname() != self.source:
-                raise ValueError("Input HDU is not a SUBINT type.")
-            else:
-                return
+            super(SubintHDU, self).__init__(header=subint_hdu.header,
+                                            data=subint_hdu.data)
 
-    @property
-    def sample_rate(self):
-        return 1.0 / (self.data_header['TBIN'] * u.s)
+    def _init_empty(self):
+        for k in self._defaults:
+            self.header.set(k[0], k[1], k[2])
+    #     self.verify(self)
+    #
+    # def verify(self):
+    #     if input_hdu.get_filename() != self.header_hdu.get_filename():
+    #         raise ValueError("Main header HDU and input HDU are not from the "
+    #                          "same file.")
+    #     else:
+    #         if input_hdu.get_extname() != self.source:
+    #             raise ValueError("Input HDU is not a SUBINT type.")
+    #         else:
+    #             return
 
-    @sample_rate.setter
-    def sample_rate(self, val):
-        if hasattr(val, 'unit'):
-            val = val.to(u.Hz).value
-        self.data_hdu['TBIN'] = 1.0 / val
-
-    @property
-    def shape(self):
-        nrows = self.data_hdu.get_nrows()
-        samples_per_row = self.data_header['NSBLK']
-        nchan = self.data_header['NCHAN']
-        npol = self.data_header['NPOL']
-        nbin = self.data_header['NBIN']
-        return (nrows * samples_per_row, nbin, npol, nchan)
-
-    def get_dim_label(self):
-        return ('time', 'phase', 'pol', 'freq')
-
-    @property
-    def pol(self):
-        return self.data_header['POL_TYPE']
-
-    @pol.setter
-    def pol(self, val):
-        self.data_header['POL_TYPE'] = val
-
-
-    @property
-    def samples_per_frame(self):
-        return self.data_header['NSBLK']
-
-    @samples_per_frame.setter
-    def samples_per_frame(self, val):
-        self.data_header['NSBLK'] = val
-
-    @property
-    def start_times(self):
-        # NOTE should we get the start time for each raw, in case the time gaps
-        # in between the rows
-        file_start = self.header_hdu.start_time
-        subint_times = u.Quantity(self.data_hdu.read_column(col='OFFS_SUB'),
-                                  u.s, copy=False)
-        samples_per_row = self.data_header['NSBLK']
-        sample_time = 1.0 / self.sample_rate
-        start_time = (file_start + subint_times[0] -
-                      self.samples_per_frame / 2 * sample_time)
-        return start_time
-
-    @start_time.setter
-    def start_time(self, time):
-        # NOTE this sets the start time of the HDU, not the file start time.
-        dt = (time - self.header_hdu.start_time.to(u.s)
-        center_off = dt + 1.0 / self.sample_rate * self.samples_per_frame / 2
-        self._buffer['OFFS_SUB'] = center_off
-
-
-    def get_freqs(self):
-        # Those are the frequency for all the rows.
-        freqs = u.Quantity(self.header_hdu.read_column(col='DAT_FREQ'),
-                           u.MHz, copy=False)
-        return freqs
-
-    def get_sideband(self):
-        # It is not clear for now.
-        return 1
-
-    def get_data(self, time_samples):
-        # The seek is not working
-        samples_per_row = self.data_header['NSBLK']
-        num_rows = int(time_samples / samples_per_row)
-        rest_samples = time_samples - num_rows * samples_per_row
-        data = self.data_hdu.read(row=np.arange(num_rows))
-        result = data['DATA'].reshape((num_rows * samples_per_row,
-                                       self.get_shape[1::]))
-        return result[0: time_samples]
+    # @property
+    # def sample_rate(self):
+    #     return 1.0 / (self.data_header['TBIN'] * u.s)
+    #
+    # @sample_rate.setter
+    # def sample_rate(self, val):
+    #     if hasattr(val, 'unit'):
+    #         val = val.to(u.Hz).value
+    #     self.data_hdu['TBIN'] = 1.0 / val
+    #
+    # @property
+    # def shape(self):
+    #     nrows = self.data_hdu.get_nrows()
+    #     samples_per_row = self.data_header['NSBLK']
+    #     nchan = self.data_header['NCHAN']
+    #     npol = self.data_header['NPOL']
+    #     nbin = self.data_header['NBIN']
+    #     return (nrows * samples_per_row, nbin, npol, nchan)
+    #
+    # def get_dim_label(self):
+    #     return ('time', 'phase', 'pol', 'freq')
+    #
+    # @property
+    # def pol(self):
+    #     return self.data_header['POL_TYPE']
+    #
+    # @pol.setter
+    # def pol(self, val):
+    #     self.data_header['POL_TYPE'] = val
+    #
+    #
+    # @property
+    # def samples_per_frame(self):
+    #     return self.data_header['NSBLK']
+    #
+    # @samples_per_frame.setter
+    # def samples_per_frame(self, val):
+    #     self.data_header['NSBLK'] = val
+    #
+    # @property
+    # def start_times(self):
+    #     # NOTE should we get the start time for each raw, in case the time gaps
+    #     # in between the rows
+    #     file_start = self.header_hdu.start_time
+    #     subint_times = u.Quantity(self.data_hdu.read_column(col='OFFS_SUB'),
+    #                               u.s, copy=False)
+    #     samples_per_row = self.data_header['NSBLK']
+    #     sample_time = 1.0 / self.sample_rate
+    #     start_time = (file_start + subint_times[0] -
+    #                   self.samples_per_frame / 2 * sample_time)
+    #     return start_time
+    #
+    # @start_time.setter
+    # def start_time(self, time):
+    #     # NOTE this sets the start time of the HDU, not the file start time.
+    #     dt = (time - self.header_hdu.start_time.to(u.s)
+    #     center_off = dt + 1.0 / self.sample_rate * self.samples_per_frame / 2
+    #     self._buffer['OFFS_SUB'] = center_off
+    #
+    #
+    # def get_freqs(self):
+    #     # Those are the frequency for all the rows.
+    #     freqs = u.Quantity(self.header_hdu.read_column(col='DAT_FREQ'),
+    #                        u.MHz, copy=False)
+    #     return freqs
+    #
+    # def get_sideband(self):
+    #     # It is not clear for now.
+    #     return 1
+    #
+    # def get_data(self, time_samples):
+    #     # The seek is not working
+    #     samples_per_row = self.data_header['NSBLK']
+    #     num_rows = int(time_samples / samples_per_row)
+    #     rest_samples = time_samples - num_rows * samples_per_row
+    #     data = self.data_hdu.read(row=np.arange(num_rows))
+    #     result = data['DATA'].reshape((num_rows * samples_per_row,
+    #                                    self.get_shape[1::]))
+    #     return result[0: time_samples]
